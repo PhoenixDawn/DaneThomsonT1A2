85d32021cc7d66e0c1007f99fe9ec931124dac8f
Dane Thomson <dane.thomson@outlook.com>
2021-07-08 12:43:44 +1000

    Added validation for contact fields

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-const form = document.querySelector("form")
-const btn = document.getElementById("button")
+let fName = document.getElementById("fname");
+let lName = document.getElementById("lname");
+let phone = document.getElementById("phone");
+let email = document.getElementById("email");
+let body = document.getElementById("body");
 
-function handleSubmit(e){
-    e.preventDefault()
-    form.submit();
-    form.reset();
-    alert("Thank you for contacting me!")
+const form = document.querySelector("form");
+const btn = document.getElementById("button");
+
+function validateInputs() {
+  if (
+    fName.value.length > 0 &&
+    email.value.length > 0 &&
+    body.value.length > 0
+  ) {
+    return true;
+  }
+  return false;
 }
 
-btn.addEventListener("click", handleSubmit)
-form.addEventListener("submit", handleSubmit)
+function handleSubmit(e) {
+  if (validateInputs()) {
+    e.preventDefault();
+    form.submit();
+    form.reset();
+    alert("Thank you for contacting me!");
+  }
+}
+
+btn.addEventListener("click", handleSubmit);
+form.addEventListener("submit", handleSubmit);
